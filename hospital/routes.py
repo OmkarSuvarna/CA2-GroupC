de3d7dd47f80f3b12bf5c0161e052c500bb1f223
OmkarSuvarna <omkar.suvarna@outlook.com>
2023-07-17 12:51:23 +0100

    added doctor profile base template, added logout functionality

--- hospital/routes.py
+++ hospital/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from hospital import app, db, bcrypt
 from hospital.forms import LoginForm, PatientForm, UserForm
 from hospital.models import User, Doctor, Patient 
+from flask_login import login_user, current_user, logout_user, login_required
 
 patient = [
     {
@@ -42,12 +43,21 @@ def admin():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.username.data == 'omkar' and form.password.data == '123':
-            flash(f'Welcome {form.username.data}', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))         
+        # if form.username.data == 'omkar' and form.password.data == '123':
+
+        #     flash(f'Welcome {form.username.data}', 'success')
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route("/register", methods=['GET', 'POST'])
 def registerPatient():
@@ -56,3 +66,9 @@ def registerPatient():
          flash(f'Added Patient : {form.firstName.data}', 'success')
          return redirect(url_for('registerPatient'))
     return render_template('registerPatient.html', form=form)
+
+@app.route("/doctor_profile")
+def doctor_profile():
+    
+    return render_template('doctor_profile.html', title ='Doctor Pofile')
+    
