05ea3740b5af2d147db326986ed324eacfe3d69b
OmkarSuvarna <omkar.suvarna@outlook.com>
2023-07-18 07:42:58 +0100

    Added routes so doctor profile can be accessed

--- run.py
+++ run.py
@@ -2,5 +2,4 @@ from hospital import app
 
 # so we do not need to set enviornment variable again, can run directly using python
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run()
+    app.run(debug=True)
