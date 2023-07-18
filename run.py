bef08475a40eff8afdd7b5f64da6b2f73035fa05
Omkar Suvarna <omkar.suvarna@outlook.com>
2023-07-18 01:25:32 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from hospital import app
 
 # so we do not need to set enviornment variable again, can run directly using python
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run()
